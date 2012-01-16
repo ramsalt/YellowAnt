869705d717ccf33fe727e57b1188848b2ae75461
Martin Normann Lund <martin@ramsalt.com>
2012-01-16 15:21:34 +0100

    Added files

---
+++ template.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @file
+ * This file is empty by default because the base theme chain (Alpha & Omega) provides
+ * all the basic functionality. However, in case you wish to customize the output that Drupal
+ * generates through Alpha & Omega this file is a good place to do so.
+ * 
+ * Alpha comes with a neat solution for keeping this file as clean as possible while the code
+ * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
+ * for more information on this topic.
+ */
+ 
+ function yellow_ant_breadcrumb($variables) {
+   $breadcrumb = $variables['breadcrumb'];
+   if (!empty($breadcrumb)) {
+     // Provide a navigational heading to give context for breadcrumb links to
+     // screen-reader users. Make the heading invisible with .element-invisible.
+     $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+ 
+     $output .= '<div class="breadcrumb">' . implode(' / ', $breadcrumb) . '</div>';
+     return $output;
+   }
+ }
